39ebffee05107acbd101393a8b080bcfff77ffa6
Tobias Gierke <tobias.gierke@code-sourcery.de>
2014-08-14 23:52:15 +0200

    changed config file default location

--- src/main/java/de/codesourcery/wotcompare/Main.java
+++ src/main/java/de/codesourcery/wotcompare/Main.java
@@ -50,7 +50,7 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
-		final File configFile = args.length == 0 ? new File("/home/tobi/.wotcompare") : new File( args[0] );
+		final File configFile = args.length == 0 ? new File(".wotcompare") : new File( args[0] );
 		new Main(configFile).run();
 	}
 
